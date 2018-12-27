3bd7d2d35269bf42fa85f7052a91320395ee8664
tsmanner <zephrincochrane@gmail.com>
2018-12-26 20:29:36 -0500

    Implemented amounts as a new node type and made `Recipe`, `Ingredient`, `Amount`, and `Step` simple specializations of the `Node` class. Introduced `NodeGroup`s to make it easier to collect several nodes and connect them as a group to another node or set of nodes.  When two `NodeGroups` are connected, all nodes in the "tail" that have no outgoing edges are connected to all nodes in the "head" that have no incoming edges.

---
+++ recipepi/graph.py
@@ -0,0 +1,113 @@
+""" RecipePi Graph Backend
+Code used to represent the backend data for recipes
+"""
+
+
+def _get_id():
+    current_id = 0
+    while True:
+        current_id += 1
+        yield current_id
+
+
+ID = _get_id()
+
+
+class Graph:
+    def __init__(self, name):
+        self.name = name
+        self.nodes = set()
+
+    def depth(self):
+        return max([item.depth() for item in self.nodes])
+
+    def graphviz(self, linesep='\n'):
+        lines = [
+            'digraph "{}" {{'.format(self.name),
+            '    label="{}"'.format(self.name.capitalize()),
+            '    labelloc=t',
+            '    fontsize=24',
+            '    rankdir=LR',
+            '    bgcolor=black',
+        ]
+        for node in sorted(self.nodes):
+            lines.append('    ' + node.graphviz())
+        edges = set()
+        for tail in self.nodes:
+            for head in tail.outgoing:
+                edges.add((tail.id, head.id))
+        for edge in sorted(edges):
+            lines.append('    {} -> {} [color=gray fontcolor=gray]'.format(*edge))
+        lines.append('}')
+        return linesep.join(lines)
+
+    def html(self):
+        pass
+
+
+class Node:
+    def __init__(self, graph: Graph):
+        super().__init__()
+        self.id = next(ID)
+        self.graph = graph
+        self.incoming = set()
+        self.outgoing = set()
+        self.graph.nodes.add(self)
+
+    def __repr__(self):
+        return "Node({})".format(self.id)
+
+    def graphviz(self):
+        raise NotImplementedError()
+
+    def html(self):
+        raise NotImplementedError()
+
+    def depth(self):
+        if self.outgoing:
+            return max([item.head.depth() for item in self.outgoing]) + 1
+        return 1
+
+    def __lt__(self, other):
+        return self.id < other.id
+
+    def __hash__(self):
+        return self.id
+
+
+class NodeGroup:
+    def __init__(self, *args):
+        self.nodes = set(args)
+
+    def __add__(self, other):
+        if isinstance(other, NodeGroup):
+            return NodeGroup(*self.nodes, *other.nodes)
+        elif isinstance(other, Node):
+            return NodeGroup(*self.nodes, other)
+
+    def __repr__(self):
+        return "NodeGroup" + str(self.nodes)
+
+
+def connect(tail, head):
+    """ Connect every node with no outgoings in `tail` to every node with no incomings in `head`
+    """
+    if isinstance(tail, Node) and isinstance(head, Node):
+        tail.outgoing.add(head)
+        head.incoming.add(tail)
+        return
+    if isinstance(tail, NodeGroup):
+        tails = [node for node in tail.nodes if not node.outgoing]
+    else:
+        tails = [tail]
+    if isinstance(head, NodeGroup):
+        heads = [node for node in head.nodes if not node.outgoing]
+    else:
+        heads = [head]
+    group = NodeGroup()
+    for t in tails:
+        for h in heads:
+            connect(t, h)
+            group += t
+            group += h
+    return group
--- recipepi/recipe.py
+++ recipepi/recipe.py
@@ -1,173 +1,127 @@
 """ Recipe
 """
-from functools import lru_cache
-import os
-
-
-def svg(item, *args, **kwargs):
-    return item.__svg__(*args, **kwargs)
-
-
-def _get_id():
-    current_id = 0
-    while True:
-        current_id += 1
-        yield current_id
-
-
-ID = _get_id()
-
-
-class Recipe:
-    def __init__(self, name):
-        self.name = name
-        self.ingredients = set()
-        self.steps = set()
-        self.edges = set()
-
-    def connect(self, tail, head):
-        self.add(tail)
-        self.add(head)
-        self.add(Edge(tail, head))
-
-    def add(self, element):
-        if isinstance(element, Ingredient):
-            self.ingredients.add(element)
-        elif isinstance(element, Step):
-            self.steps.add(element)
-        elif isinstance(element, Edge):
-            self.edges.add(element)
-
-    def graphviz(self, linesep='\n'):
-        lines = [
-            'digraph "{}" {{'.format(self.name),
-            '    label="{}"'.format(self.name.capitalize()),
-            '    labelloc=t',
-            '    fontsize=24',
-            '    rankdir=LR',
-            '    bgcolor=black',
-        ] + [
-            '    ' + graphviz(recipe_element)
-            for recipe_element in sorted(self.ingredients | self.steps | self.edges)
-        ] + [
-            '}'
-        ]
-        return linesep.join(lines)
-
-
-@lru_cache()
-def get_recipe(recipe_name):
-    return Recipe(recipe_name)
-
-
-class Node(dict):
-    def __init__(self, recipe: Recipe):
-        super().__init__()
-        self.id = next(ID)
-        self["recipe"] = recipe
-        self["id"] = self.id
-        self["edges"] = set()
-        self["format"] = "Node({id})"
-        self["graphviz_format"] = '"{id}" [label="Node({id})"]'
-        recipe.add(self)
-
-    def __lt__(self, other):
-        return self.id < other.id
-
-    def __hash__(self):
-        return self.id
-
-    def __str__(self):
-        return self["format"].format(**self)
-
-
-class Edge(dict):
-    def __init__(self, tail: "Node", head: "Node"):
-        super().__init__()
-        self.id = next(ID)
-        self.tail = tail
-        self["tail"] = tail.id
-        tail["edges"].add(self)
-        self.head = head
-        self["head"] = head.id
-        head["edges"].add(self)
-        self["graphviz_format"] = '"{tail}" -> "{head}" [color=gray]'
-
-    def __lt__(self, other):
-        return self.id < other.id
-
-    def __hash__(self):
-        return self.id
-
-    def __str__(self):
-        return " -> "
+from typing import Iterable, Union
+from graph import Graph, Node, NodeGroup, connect
 
 
 class Ingredient(Node):
-    def __init__(self, recipe, name, amount):
+    def __init__(self, recipe: "Recipe", name):
         super().__init__(recipe)
-        self["name"] = name
-        self["amount"] = amount
-        self["graphviz_format"] = '"{id}" [color=gray fontcolor=gray shape=rectangle label="{name} {amount}"]'
+        self.name = name
+
+    def graphviz(self):
+        return '"{}" [shape=rectangle color=gray fontcolor=gray label="{}"]'.format(self.id, self.name)
+
+    def __repr__(self):
+        return "Ingredient({})".format(self.name)
+
+
+class Amount(Node):
+    def __init__(self, recipe: "Recipe", amount: Union[str, int], modifies: Node = None):
+        super().__init__(recipe)
+        self.amount = amount
+        if modifies:
+            connect(modifies, self)
+
+    def graphviz(self):
+        return '"{}" [shape=rectangle color=gray fontcolor=gray label="{}"]'.format(self.id, self.amount)
+
+    def __repr__(self):
+        return "Amount({})".format(self.amount)
 
 
 class Step(Node):
-    def __init__(self, recipe, procedure, *inputs):
+    def __init__(self, recipe: "Recipe", procedure):
         super().__init__(recipe)
-        self["procedure"] = procedure
-        self["graphviz_format"] = '"{id}" [color=gray fontcolor=gray shape=rectangle label="{procedure}"]'
-        [recipe.connect(item, self) for item in inputs]
+        self.procedure = procedure
+
+    def graphviz(self):
+        return '"{}" [shape=rectangle color=gray fontcolor=gray label="{}"]'.format(self.id, self.procedure)
+
+    def __repr__(self):
+        return "Step({})".format(self.procedure)
 
 
-def graphviz(element):
-    return element["graphviz_format"].format(**element)
+class Recipe(Graph):
+    def __init__(self, name: str):
+        super().__init__(name)
+
+    def __repr__(self):
+        lines = [
+            'Recipe: {}'.format(self.name),
+        ]
+        for node in self.nodes:
+            lines.append('{}'.format(node))
+        edges = set()
+        for tail in self.nodes:
+            for head in tail.outgoing:
+                edges.add((tail.id, head.id))
+        for edge in sorted(edges):
+            lines.append('{} -> {}'.format(*edge))
+        return "\n    ".join(lines)
+
+    def ingredient(self, name, amount):
+        ingredient = Ingredient(self, name)
+        amount = Amount(self, amount, ingredient)
+        return NodeGroup(ingredient, amount)
+
+    def step(self, procedure, *inputs):
+        step = Step(self, procedure)
+        for ipt in inputs:
+            if isinstance(ipt, Iterable):
+                [connect(item, step) for item in ipt]
+            else:
+                connect(ipt, step)
+        return step
 
 
 if __name__ == '__main__':
     import subprocess as sp
-    r = get_recipe("Tom's Chili")
-    gb = Ingredient(r, "Ground Beef", "1/2 Lb")
-    gp = Ingredient(r, "Ground Pork", "1/4 Lb")
-    gv = Ingredient(r, "Ground Veal", "1/4 Lb")
-    w = Ingredient(r, "Water", "1/2 Cup")
+    r = Recipe("Tom's Chili")
+    meats = NodeGroup()
+    meats += r.ingredient("Ground Beef", "1/2 Lb")
+    meats += r.ingredient("Ground Pork", "1/4 Lb")
+    meats += r.ingredient("Ground Veal", "1/4 Lb")
+    meats += r.ingredient("Water", "1/2 Cup")
+    print(meats)
 
-    pt = Ingredient(r, "Plum Tomato", "2")
-    oo = Ingredient(r, "Olive Oil", "1/4 Cup")
+    ptoo = NodeGroup()
+    ptoo += r.ingredient("Plum Tomato", "2")
+    ptoo += r.ingredient("Olive Oil", "1/4 Cup")
 
-    rbp = Ingredient(r, "Red Bell Peper", "1")
-    obp = Ingredient(r, "Orange Bell Pepper", "1")
-    mp = Ingredient(r, "Mild Pepper", "1")
-    yo = Ingredient(r, "Yellow Onion", "2")
-    g = Ingredient(r, "Garlic", "1/2 Head")
+    veggies = NodeGroup()
+    veggies += r.ingredient("Red Bell Peper", "1")
+    veggies += r.ingredient("Orange Bell Pepper", "1")
+    veggies += r.ingredient("Mild Pepper", "1")
+    veggies += r.ingredient("Yellow Onion", "2")
+    veggies += r.ingredient("Garlic", "1/2 Head")
 
-    black_beans = Ingredient(r, "Black Beans", "1 Can")
-    kidney_beans = Ingredient(r, "Kidney Beans", "1 Can")
-    corn = Ingredient(r, "Sweet Corn", "1 Can")
+    cans = NodeGroup()
+    cans += r.ingredient("Black Beans", "1 Can")
+    cans += r.ingredient("Kidney Beans", "1 Can")
+    cans += r.ingredient("Sweet Corn", "1 Can")
 
-    chili_powder = Ingredient(r, "Chili Powder", "3 Tbsp")
-    basil_powder = Ingredient(r, "Basil Powder", "3 Tbsp")
-    paprika = Ingredient(r, "Paprika", "1 Tsp")
-    cayenne_powder = Ingredient(r, "Cayenne Powder", "1 Tsp")
-    salt = Ingredient(r, "Salt", "To Taste")
+    seasonings = NodeGroup()
+    seasonings += r.ingredient("Chili Powder", "3 Tbsp")
+    seasonings += r.ingredient("Basil Powder", "3 Tbsp")
+    seasonings += r.ingredient("Paprika", "1 Tsp")
+    seasonings += r.ingredient("Cayenne Powder", "1 Tsp")
+    seasonings += r.ingredient("Salt", "To Taste")
 
-    meat_mix = Step(r, "Mix in pot over medium heat and brown", gb, gp, gv, w)
-    puree = Step(r, "Puree", pt, oo)
-    dice = Step(r, "Dice", rbp, obp, mp, yo, g)
-    veggie_mix = Step(r, "Combine and simmer for 30 minutes or until vegetables soften", meat_mix, puree, dice)
-    drain = Step(r, "Open, draining most of the fluid", black_beans, kidney_beans, corn)
-    mix = Step(
-        r,
+    meat_mix = r.step("Mix in pot over medium heat and brown", meats)
+    puree = r.step("Puree", ptoo)
+    dice = r.step("Dice", veggies)
+    veggie_mix = r.step("Combine and simmer for 30 minutes or until vegetables soften", meat_mix, puree, dice)
+    drain = r.step("Open, draining most of the fluid", cans)
+    mix = r.step(
         "Mix in pot and simmer for at least another 30 minutes.  Longer is typically better",
-        chili_powder,
-        basil_powder,
-        paprika,
-        cayenne_powder,
-        salt,
+        seasonings,
         veggie_mix,
         drain,
     )
 
-    print(r.graphviz())
     with open("rawr.dot", "w") as dotfile:
         dotfile.write(r.graphviz())
     sp.run('dot -Tpng -o rawr.png rawr.dot', shell=True)
+
+    print(r)
